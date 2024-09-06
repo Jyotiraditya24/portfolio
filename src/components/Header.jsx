2d048daefdb922be40bcc8251e3510b7b0f3b2a8
jyotiraditya <jyotiraditya24@gmail.com>
2024-09-06 23:26:07 +0530

    A little more improvement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex flex-row justify-between bg-purple-500 text-white pl-7 pr-10 py-5 rounded-xl shadow-xl mb-5">
+    <div className="flex flex-row justify-between bg-purple-500 text-black pl-7 pr-10 py-5 rounded-xl shadow-xl mb-5">
       <div className="basis-1/3">
         <h1 className="font-asil text-xl font-extrabold">Jyotir Aditya </h1>
       </div>
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,16 +3,17 @@ import React from "react";
 const Landing = () => {
   return (
     <div className="w-full grid grid-cols-12">
-      <div className="w-full grid col-span-8 grid-cols-8">
-        <div className="col-span-5 bg-purple-500 rounded-xl pl-8 flex flex-col">
+      <div className="w-full grid col-span-8 grid-cols-8 lg:gap-x-10 2xl:gap-x-16">
+        <div className="col-span-5 bg-purple-500 rounded-xl pl-8 flex flex-col pt-2 pb-10 gap-y-10">
           <div className="flex flex-row w-full justify-end items-center p-5">
             <div className="rounded-full h-[200px] w-[200px] bg-white"></div>
           </div>
-          <div>
-            Full Stack Web Developer
-          </div>
+          <div className="text-5xl font-playwrite text">Full Stack</div>
+          <div className="text-5xl font-playwrite">Web Developer</div>
         </div>
-        <div className="col-span-3"></div>
+        <div className="col-span-3 bg-purple-500 rounded-xl">
+
+        </div>
       </div>
       <div className="col-span-4"></div>
     </div>
